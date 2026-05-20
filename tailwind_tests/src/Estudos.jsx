237f6e47d5df294f553acddb365354629998ba33
Pedro Heinrich <pedroheinrich91@gmail.com>
2026-05-20 15:26:05 -0300

    feat: treina tailwind css com card de aviso, card de perfil e login simples

--- tailwind_tests/src/Estudos.jsx
+++ tailwind_tests/src/Estudos.jsx
@@ -3,7 +3,7 @@ import "./index.css";
 function Estudos() {
   return (
     <>
-      <div className="gap-20 flex flex-col">
+      <div className="gap-10 flex flex-col">
         <section id="colors" className="flex flex-col gap-2">
           <h1 className="text-3xl text-red-700">exemplo de cor vermelha</h1>
           <h2 className="text-[#006465]">mudando a cor pelo rgb #006465</h2>
@@ -37,6 +37,66 @@ function Estudos() {
          <h1 className="text-white w-36 bg-amber-900 truncate">texto qualquer que nao vai couber e vai quebrar</h1>
          <h2 className="text-white w-46 bg-amber-400 line-clamp-3">texto qualquer que nao vai couber e vai quebrar na terceira linha porque é gigantesco.</h2>
         </section>
+
+        <section id="BORDA" className=" gap-8 flex justify-center p-5 bg-black">
+          <div className="w-16 h-16 bg-gray-700 rounded-xl border-l-blue-500 border-t-red-500 border-r-green-500"></div>
+          <div className="w-16 h-16 bg-gray-700 rounded-xl border-t border-amber-500 "></div>
+          <div className="w-16 h-16 bg-gray-700 rounded-xl border-r border-amber-500"></div>
+          <div className="w-16 h-16 bg-gray-700 rounded-xl border-b border-amber-500"></div>
+          <div className="w-16 h-16 bg-gray-700 rounded-xl border-l border-amber-500"></div>
+        </section>
+
+        <section id="butoes" className="flex justify-center">
+          <button className="text-white bg-indigo-600 rounded-md ml-8 px-6 py-2 cursor-pointer border-none">Clique Aqui</button>
+        </section>
+
+        <div className="bg-gray-900 p-20">
+          <section id="aviso" className="flex justify-center items-center rounded-md ml-2 gap-1 bg-[#FCF8C2] w-80 py-2 border-l-2 border-l-amber-300">
+            <h2 className="text-amber-700">Você não tem mais acesso.</h2>
+            <h2 className="text-amber-700 underline cursor-pointer">Faça upgrade!</h2>
+         </section>
+        </div>
+
+        <section className="bg-gray-900 p-20">
+          <div id="card" className="bg-white py-8 px-5 rounded-lg flex flex-col gap-2">
+            <div id="perfil" className="flex">
+             <img className="h-16 rounded-[360px]" src="/perfil.png" alt="" />
+              <div id="textos" className="ml-2 flex flex-col justify-center">
+                <h4 className="font-medium">João Silva</h4>
+                <p className="text-gray-400 line-clamp-2" >Te mandou um convite!</p>
+              </div>
+            </div>
+
+              <div id="botoes" className="flex gap-2">
+                <button className="bg-blue-600 text-white p-2 rounded-md">Aceitar</button>
+                <button className="border border-gray-400  p-2 rounded-md">Negar</button>
+              </div>
+          </div>
+        </section>
+
+
+        <section className="bg-gray-900 p-20 flex flex-col items-center gap-8">
+            <h1 className="text-3xl text-white text-center font-bold">Sistema de login</h1>
+            <div>
+              <p className="text-white mb-1">Endereço de Email</p>
+              <input type="text" className="w-80 h-10 outline outline-gray-600 bg-[#1F2936] rounded-md"/>
+            </div>        
+            <div>
+              <p className="text-white mb-1">Senha</p>
+              <input type="text" className="w-80 h-10 outline outline-gray-600 bg-[#1F2936] rounded-md"/>
+            </div>        
+
+            <button className="bg-[#ECB303] font-bold text-white p-2 w-80 rounded-md">Entrar</button>
+        
+            <div className="flex gap-1">
+              <p className="text-gray-500">não é membro?</p>
+              <a className="text-amber-400" href="#">Faça seu cadastro agora</a>
+            </div>
+        
+        </section>
+
+
+        
       </div>
     </>
   );
