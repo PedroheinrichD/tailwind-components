3dc0bd75caa6086290408a4c7a1addc708458d47
Pedro Heinrich <pedroheinrich91@gmail.com>
2026-05-21 15:15:28 -0300

    feat: adiciona exemplo para css grid

---
+++ tailwind_tests/src/Estudos2.jsx
@@ -0,0 +1,19 @@
+function Estudos2(){
+    return(
+        <>
+            <section id="responsividade" className="p-4 bg-black grid grid-cols-3 gap-3 ">
+                    <div className="p-6  bg-amber-400 rounded-2xl col-span-full">1</div>
+                    <div className="p-6  bg-amber-400 rounded-2xl col-span-2">2</div>
+                    <div className="p-6  bg-amber-400 rounded-2xl">3</div>
+                    <div className="p-6  bg-amber-400 rounded-2xl">4</div>
+                    <div className="p-6  bg-amber-400 rounded-2xl col-start-2 col-end-4">5</div>
+                    <div className="p-6  bg-amber-400 rounded-2xl">6</div>
+                    <div className="p-6  bg-amber-400 rounded-2xl row-span-2">7</div>
+                    <div className="p-6  bg-amber-400 rounded-2xl row-span-2">8</div>
+                    <div className="p-6  bg-amber-400 rounded-2xl">9</div>
+            </section>
+        </>
+    )
+}
+
+export default Estudos2
--- tailwind_tests/src/main.jsx
+++ tailwind_tests/src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Estudos from "./Estudos.jsx";
+import Estudos2 from "./Estudos2.jsx";
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Estudos />
+    <Estudos2 />
   </StrictMode>,
 )
