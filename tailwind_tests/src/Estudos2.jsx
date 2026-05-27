446249371a86f41bd27a2b601e9eb8b9ae87c891
Pedro Heinrich <pedroheinrich91@gmail.com>
2026-05-27 14:22:53 -0300

    feat: Implementa icones svg

--- tailwind_tests/src/Estudos2.jsx
+++ tailwind_tests/src/Estudos2.jsx
@@ -2,7 +2,12 @@ function Estudos2(){
     return(
         <>
             <section id="background">
-                <div className="transition-all duration-1000 w-40 h-24 bg-sky-500 hover:w-full hover:bg-purple-500 ">
+                <div className="transition-all duration-1000 w-40 h-24 bg-sky-500 hover:w-full hover:bg-purple-500 flex justify-center items-center">
+                    <svg className="hover:stroke-black h-10 fill-amber-600 stroke-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="m11.25 9-3 3m0 0 3 3m-3-3h7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                    </svg>
+
+
                 </div>
             </section>
         </>
