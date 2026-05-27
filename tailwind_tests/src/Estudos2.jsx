4f57345b8584f4808aba17656da1bf771af11cfe
Pedro Heinrich <pedroheinrich91@gmail.com>
2026-05-27 16:58:50 -0300

    feat: Adiciona ciculo de carregamento usando bordas e animate-spin do proprio tailwind

--- tailwind_tests/src/Estudos2.jsx
+++ tailwind_tests/src/Estudos2.jsx
@@ -1,14 +1,13 @@
 function Estudos2(){
     return(
         <>
-            <section id="background">
+            <section id="background" className="flex flex-col gap-20 p-5">
                 <div className="transition-all duration-1000 w-40 h-24 bg-sky-500 hover:w-full hover:bg-purple-500 flex justify-center items-center">
-                    <svg className="hover:stroke-black h-10 fill-amber-600 stroke-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <svg className="h-10 fill-amber-600 stroke-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="m11.25 9-3 3m0 0 3 3m-3-3h7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                     </svg>
-
-
                 </div>
+                 <div id="circle" className="border-[#BCD9FB] border-8 border-l-blue-800 w-12 h-12 rounded-[360px] animate-spin" ></div>
             </section>
         </>
     )
