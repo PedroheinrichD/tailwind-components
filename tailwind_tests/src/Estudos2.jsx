cb5d309fa0c92c03be0a073006507c9a286d0907
Pedro Heinrich <pedroheinrich91@gmail.com>
2026-05-27 14:10:25 -0300

    feat: adiciona hover com tailwind CSS

--- tailwind_tests/src/Estudos2.jsx
+++ tailwind_tests/src/Estudos2.jsx
@@ -2,7 +2,7 @@ function Estudos2(){
     return(
         <>
             <section id="background">
-                <div className="w-screen h-screen bg-gradient-to-tr from-sky-500 to-indigo-500 ">
+                <div className="transition-all duration-1000 w-40 h-24 bg-sky-500 hover:w-full hover:bg-purple-500 ">
                 </div>
             </section>
         </>
