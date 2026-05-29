06964e6c88cb1f5612cf234e1e010e9b59263f98
Pedro Heinrich <pedroheinrich91@gmail.com>
2026-05-29 07:09:15 -0300

    feat: adiciona grid com responsividade com exemplo de fotos

--- tailwind_tests/src/Estudos2.jsx
+++ tailwind_tests/src/Estudos2.jsx
@@ -1,61 +1,21 @@
 function Estudos2() {
   return (
     <>
-      <section
-        id="background"
-        className="flex flex-col gap-20 p-5 items-center"
-      >
-        <div className="transition-all duration-1000 w-40 h-24 bg-sky-500 hover:w-full hover:bg-purple-500 flex justify-center items-center">
-          <svg
-            className="h-10 fill-amber-600 stroke-white"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="size-6"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="m11.25 9-3 3m0 0 3 3m-3-3h7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-            />
-          </svg>
+      <section id="Grid-Responsivo" className="container mx-auto">
+        <div id="house" className="grid p-2 gap-2 md:grid-cols-3 md:grid-rows-2">
+          <div className="md:col-span-2 row-span-2 bg-[url(https://images.unsplash.com/photo-1583608205776-bfd35f0d9f83?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGhvdXNlfGVufDB8fDB8fHww)] bg-cover bg-center h-64 md:h-full bg-bg-no-repeat"></div>
+          <div className="bg-[url(https://plus.unsplash.com/premium_photo-1661962841993-99a07c27c9f4?q=80&w=1331&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] bg-cover bg-center h-64 bg-bg-no-repeat"></div>
+          <div className="md:col-start-3 bg-[url(https://images.unsplash.com/photo-1617228069096-4638a7ffc906?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] bg-cover bg-center h-64 bg-bg-no-repeat"></div>
         </div>
-        <div
-          id="circle"
-          className="border-[#BCD9FB] border-8 border-l-blue-800 w-12 h-12 rounded-[360px] animate-spin"
-        ></div>
-      </section>
-
-      <section id="card" className="">
-        <div
-          id="heroCard"
-          className="container mx-auto bg-[#1D293B] rounded-2xl flex flex-col justify-center items-center px-10 py-5 gap-2 md:flex-row md:justify-start md:p-0"
-        >
-          <div>
-            <img
-              className="w-20 h-20 rounded-full md:rounded-none md:w-40 md:h-40 md:rounded-l-2xl / lg:w-60 lg:h-60"
-              src="https://images.unsplash.com/photo-1709148910274-b2cb811c0a2b?q=80&w=762&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              alt=""
-            />
-          </div>
-          <div id="texts">
-            <p className="text-white text-center lg:text-xl">
-              "Me fale e eu esqueço. Me ensine e eu lembro. Me envolva e eu
-              aprendo de verdade."
-            </p>
-            <div id="text-name" className="text-center">
-              <a
-                className="text-sky-400 font-semibold hover:animate-pulse"
-                href="https://pt.wikipedia.org/wiki/Benjamin_Franklin"
-              >
-                Benjamin Franklin
-              </a>
-              <h4 className="text-gray-500">Político, EUA</h4>
-            </div>
-          </div>
+        <div id="information" className="p-2">
+          <h1 className="font-semibold text-3xl mb-3">Título da casa</h1>
+          <ul id="badges" className="flex gap-3 ">
+            <li className="bg-green-600 text-white rounded-full px-3 py-0.5">Campo</li>
+            <li className="bg-green-600 text-white rounded-full px-3 py-0.5">Moderno</li>
+            <li className="bg-green-600 text-white rounded-full px-3 py-0.5">Piscina</li>
+          </ul>
         </div>
+        
       </section>
     </>
   );
